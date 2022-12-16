5bf8e0b69f895caed0c10f0d07913f5d7d0a3ae2
BusyBee15 <104816304+BusyBee15@users.noreply.github.com>
2022-12-16 23:07:57 +0530

    starter code for the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {StreamChat} from 'stream-chat'
+import  {
+     Chat,
+     Channel,
+     Window,
+     ChannelHeader,
+     MessageList,
+     MessageInput,
+     Thread,
+     LoadingIndicator
+} from 'stream-chat-react'
+
+import 'stream-chat-react/dist/css/index.css'
+
+const apiKey = process.env.REACT_APP_STREAM_API_KEY
+
+const user = {
+  id : 'john',
+  name : 'John',
+  image : "https://getstream.imgix.net/images/random_svg/FS.png"
 }
 
-export default App;
+function App() {
+  const [client, setClient] = useState(null)
+  const [channel, setChannel] = useState(null)
+
+  useEffect(()=>{
+      async function init(){
+        const chatClient = StreamChat.getInstance(apiKey)
+
+        await chatClient.connectUser(user, chatClient.devToken(user.id))
+
+        const channel = chatClient.channel('messaging', 'react-talk', {
+          // image: 'https://www.drupal.org/files/project-images/react.png',
+          name :'Talk about react',
+          members : [user.id]
+        })
+
+        await channel.watch()
+
+        setChannel(channel)
+        setClient(chatClient)
+
+      }
+
+      if(client) return () => client.disconnectUser()
+
+      init()
+  }, [])
+
+   if(!channel || !client) return <LoadingIndicator/>
+
+   return (
+    <Chat client={client} theme="messaging light">
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader/>
+          <MessageList/>
+          <MessageInput/>
+        </Window>
+        <Thread/>
+      </Channel>
+    </Chat>
+   )
+
+}
+
+export default App
