fa612c1168b807b1f23735daf8b4444a38544ed9
BusyBee15 <104816304+BusyBee15@users.noreply.github.com>
2022-12-17 16:31:50 +0530

    dummy data for use

---
+++ src/Data.js
@@ -0,0 +1,28 @@
+export const data = [
+    {
+      id: 1,
+      title: "Scholarship title",
+      image: "https://images.pexels.com/photos/87452/flowers-background-butterflies-beautiful-87452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic tempora obcaecati excepturi atque voluptas perspiciatis ullam commodi fuga alias dolores!"
+
+    },
+    {id: 2,
+       title: "Scholarship title",
+      image:'https://images.pexels.com/photos/107606/pexels-photo-107606.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic tempora obcaecati excepturi atque voluptas perspiciatis ullam commodi fuga alias dolores!"  
+
+
+    },
+    {id: 3,
+       title: "Scholarship title",
+    image:'https://images.pexels.com/photos/65622/flower-blossom-bloom-in-the-shadow-65622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic tempora obcaecati excepturi atque voluptas perspiciatis ullam commodi fuga alias dolores!"  
+    },
+    
+    {id: 4,
+       title: "Scholarship title",
+    image:'https://images.pexels.com/photos/67642/blossom-bloom-flower-blue-67642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic tempora obcaecati excepturi atque voluptas perspiciatis ullam commodi fuga alias dolores!" 
+}
+  
+]
