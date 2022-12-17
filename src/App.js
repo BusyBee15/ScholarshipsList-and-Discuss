f1f5313fb86720cbe9e9131d20682101d21c09de
BusyBee15 <104816304+BusyBee15@users.noreply.github.com>
2022-12-17 16:37:30 +0530

    some extra code

--- src/App.js
+++ src/App.js
@@ -1,70 +1,17 @@
-import React, {useEffect, useState} from 'react'
-
-import {StreamChat} from 'stream-chat'
-import  {
-     Chat,
-     Channel,
-     Window,
-     ChannelHeader,
-     MessageList,
-     MessageInput,
-     Thread,
-     LoadingIndicator
-} from 'stream-chat-react'
-
-import 'stream-chat-react/dist/css/index.css'
-
-const apiKey = process.env.REACT_APP_STREAM_API_KEY
-
-const user = {
-  id : 'john',
-  name : 'John',
-  image : "https://getstream.imgix.net/images/random_svg/FS.png"
-}
+import React from 'react'
+import Header from './Header'
+import List from './List'
+import Footer from './Footer'
+import Contact from './Contact'
 
 function App() {
-  const [client, setClient] = useState(null)
-  const [channel, setChannel] = useState(null)
-
-  useEffect(()=>{
-      async function init(){
-        const chatClient = StreamChat.getInstance(apiKey)
-
-        await chatClient.connectUser(user, chatClient.devToken(user.id))
-
-        const channel = chatClient.channel('messaging', 'react-talk', {
-          // image: 'https://www.drupal.org/files/project-images/react.png',
-          name :'Talk about react',
-          members : [user.id]
-        })
-
-        await channel.watch()
-
-        setChannel(channel)
-        setClient(chatClient)
-
-      }
-
-      if(client) return () => client.disconnectUser()
-
-      init()
-  }, [])
-
-   if(!channel || !client) return <LoadingIndicator/>
-
-   return (
-    <Chat client={client} theme="messaging light">
-      <Channel channel={channel}>
-        <Window>
-          <ChannelHeader/>
-          <MessageList/>
-          <MessageInput/>
-        </Window>
-        <Thread/>
-      </Channel>
-    </Chat>
-   )
-
+  return (
+   <>
+    <Header/>
+    <Contact/>
+    <Footer/>
+   </>
+  )
 }
 
 export default App
